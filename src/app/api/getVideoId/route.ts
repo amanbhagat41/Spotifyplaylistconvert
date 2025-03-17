fa2a9867f257a312494900c23ae60c2ae4a88a47
amanbhagat41 <34489796+amanbhagat41@users.noreply.github.com>
2025-03-17 17:03:44 -0400

    Vercel fix (PLEASE BE THE LAST ONE)

--- src/app/api/getVideoId/route.ts
+++ src/app/api/getVideoId/route.ts
@@ -14,7 +14,7 @@ export async function GET(req: NextRequest) {
         );
     }
 
-    return new Promise((resolve) => {
+    return new Promise<Response>((resolve) => {
         // Use yt-dlp to search for the video and extract the video ID
         const command = `yt-dlp "ytsearch:${query}" --get-id`;
 
